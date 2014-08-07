3ff20d2b53487b2e1c487b3c2547c091c7fd6d85
vic <vic@vicdemac-mini.lan>
2014-08-07 15:23:47 +0800

    Initial Commit

---
+++ libSqliteDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  libSqliteDemo
+//
+//  Created by vic on 14-8-7.
+//  Copyright (c) 2014年 vic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ libSqliteDemoTests/libSqliteDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  libSqliteDemoTests.m
+//  libSqliteDemoTests
+//
+//  Created by vic on 14-8-7.
+//  Copyright (c) 2014年 vic. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface libSqliteDemoTests : XCTestCase
+
+@end
+
+@implementation libSqliteDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
